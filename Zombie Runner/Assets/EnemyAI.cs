7d31d40bd3372b46e901580f1e691ceb6ec35879
spiegan77 <stephenpiegan@outlook.com>
2022-05-31 06:23:49 -0400

    Got A Gun

--- Zombie Runner/Assets/EnemyAI.cs
+++ Zombie Runner/Assets/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,12 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;
+    //public float enemyAIRadius = 5f;
+    bool isProvoked = false;
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
+
 
     void Start()
     {
@@ -19,9 +23,44 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if(distanceToTarget <= chaseRange)
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
         }
+        else if(distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            Debug.Log("Fucking Stop");
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    void AttackTarget()
+    {
+        Debug.Log("Shoot Target");
+    }
+
+    void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 1, 0, 0.25f);
+        //Gizmos.DrawSphere(transform.position, enemyAIRadius);
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
