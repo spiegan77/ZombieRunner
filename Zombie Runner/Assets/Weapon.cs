adfb9f4e35b2b6d63b812e65c833a86ff37232db
spiegan77 <stephenpiegan@outlook.com>
2022-06-01 18:54:09 -0400

    MuzzleFlash

--- Zombie Runner/Assets/Weapon.cs
+++ Zombie Runner/Assets/Weapon.cs
@@ -7,6 +7,9 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100;
+    [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
+
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -17,8 +20,26 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        Debug.Log($"I hit {hit.transform.name}");
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log($"I hit {hit.transform.name}");
+            //TODO: Add some sort of hit effect for visual players
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else { return; }
     }
 }
