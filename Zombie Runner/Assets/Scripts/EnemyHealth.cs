44da678c984bac3fd0dc289ee12db78e1053969c
spiegan77 <stephenpiegan@outlook.com>
2022-06-11 20:51:24 -0400

    Zombie

--- Zombie Runner/Assets/Scripts/EnemyHealth.cs
+++ Zombie Runner/Assets/Scripts/EnemyHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,13 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] float hitPoints = 100f;
 
+    bool isDead = false;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
     // Create a public method which reduces HP by amount of damage
     public void TakeDamage(float damage)
     {
@@ -13,7 +21,17 @@ public class EnemyHealth : MonoBehaviour
         hitPoints -= damage;
         if (hitPoints <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
+    }
+
+    private void Die()
+    {
+        if (isDead)
+        {
+            return;
+        }
+        isDead = true;
+        GetComponent<Animator>().SetTrigger("die");
     }
 }
